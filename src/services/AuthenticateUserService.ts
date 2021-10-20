6c0ff5268b05c67e88c3a0ada51e9262084643d9
Yanne <nanefc@hotmail.com>
2021-10-20 16:01:11 -0300

    second commit

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,9 +1,13 @@
 import axios from 'axios';
 
+interface IAccessTokenResponse {
+  access_token: string
+}
+
 class AuthenticateUserService {
   async execute(code: string) {
     const url = "https://github.com/login/oauth/access_token";
-    const response = await axios.post(url, null, {
+    const { data: accessTokenResponse } = await axios.post<IAccessTokenResponse>(url, null, {
       params: {
         client_id: process.env.GITHUB_CLIENT_ID,
         client_secret: process.env.GITHUB_CLIENT_SECRET,
@@ -12,7 +16,7 @@ class AuthenticateUserService {
       headers: {
         "Accept": "application/json"
       }
-    })
+    });
     return response.data;
   }
 }
